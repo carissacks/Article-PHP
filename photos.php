140b65c1147b960b0d922f2ee3a4d26f7a8ce273
Kenny <kenny77beast@users.noreply.github.com>
2019-10-05 17:45:41 +0700

    Photos Update

--- photos.php
+++ photos.php
@@ -35,81 +35,61 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-9 col-12">
-                    <div class="row m-3 justify-content-center">
-                        <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
-                            <?php
-                                if($result['content'])
-                                    echo $result['content'];
-                            ?>                            
-                        </div>
-                        <div class="col-9 m-2 p-0">
-                            <?php
-                                if($result['cover_img'])
-                                    echo "<img class='img-fluid' src=".$result['cover_img']." alt='cover-img'>";
-                            ?>
-                        </div>
-                    </div>
+                    <?php if($result['content']) : ?>
+                        <div class="row m-3 justify-content-center">
+                            <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
+                                <?php echo $result['content']; ?>
+                            </div>
 
-                    <div class="row m-3 justify-content-center">
-                        <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
-                            <?php
-                                if($result['content_2'])
-                                    echo $result['content_2'];
-                            ?>
+                            <div class="col-9 m-2 p-0">
+                                <img class="img-fluid" src="<?php echo $result['cover_img'] ?>" alt="cover-img">
+                            </div>
                         </div>
-                        <div class="col-9 m-2 p-0">
-                            <?php
-                                if($result['image_2'])
-                                    echo "<img class='img-fluid' src=".$result['image_2']." alt='cover-img'>";
-                            ?>
-                        </div>
-                    </div>
+                    <?php endif; ?>
 
-                    <div class="row m-3 justify-content-center">
-                        <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
-                            <?php
-                                if($result['content_3'])
-                                    echo $result['content_3'];
-                            ?>
+                    <?php if($result['content_2']) : ?>
+                        <div class="row m-3 justify-content-center">
+                            <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
+                                <?php echo $result['content_2']; ?>
+                            </div>
+                            <div class="col-9 m-2 p-0">
+                                <img class='img-fluid' src="<?php echo $result['image_2'] ?>" alt='cover-img'>
+                            </div>
                         </div>
-                        <div class="col-9 m-2 p-0">
-                            <?php
-                                if($result['image_3'])
-                                    echo "<img class='img-fluid' src=".$result['image_3']." alt='cover-img'>";
-                            ?>
+                    <?php endif; ?>
+                        
+                    <?php if($result['content_3']) : ?>
+                        <div class="row m-3 justify-content-center">
+                            <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
+                                <?php echo $result['content_3']; ?>
+                            </div>
+                            <div class="col-9 m-2 p-0">
+                                <img class='img-fluid' src="<?php echo $result['image_3'] ?>" alt='cover-img'>
+                            </div>
                         </div>
-                    </div>
-                    
-                    <div class="row m-3 justify-content-center">
-                        <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
-                            <?php
-                                if($result['content_4'])
-                                    echo $result['content_4'];
-                            ?>
-                        </div>
-                        <div class="col-9 m-2 p-0">
-                            <?php
-                                if($result['image_4'])
-                                    echo "<img class='img-fluid' src=".$result['image_4']." alt='cover-img'>";
-                            ?>
-                        </div>
-                    </div>
+                    <?php endif; ?>
 
-                    <div class="row m-3 justify-content-center">
-                        <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
-                            <?php
-                                if($result['content_5'])
-                                    echo $result['content_5'];
-                            ?>
+                    <?php if($result['content_4']) :?>
+                        <div class="row m-3 justify-content-center">
+                            <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
+                                <?php echo $result['content_4']; ?>
+                            </div>
+                            <div class="col-9 m-2 p-0">
+                                <img class='img-fluid' src="<?php echo $result['image_4'] ?>" alt='cover-img'>
+                            </div>
                         </div>
-                        <div class="col-9 m-2 p-0">
-                            <?php
-                                if($result['image_5'])
-                                    echo "<img class='img-fluid' src=".$result['image_5']." alt='cover-img'>";
-                            ?>
+                    <?php endif; ?>
+
+                    <?php if($result['content_5']) :?>
+                        <div class="row m-3 justify-content-center">
+                            <div class="m-2 p-0" style="width: 100px; word-break: break-all; text-align:right">
+                                <?php echo $result['content_5']; ?>
+                            </div>
+                            <div class="col-9 m-2 p-0">
+                                <img class='img-fluid' src="<?php echo $result['image_5'] ?>" alt='cover-img'>
+                            </div>
                         </div>
-                    </div>
-                    
+                    <?php endif; ?>
 
                     <div class="row justify-content-start">
                         <div class="col-12">                            
